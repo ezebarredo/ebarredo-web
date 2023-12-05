bb0fdc6f2b1118e0cd66f3de3e04d9f188dea1df
ezebarredo <ezequielbarredo@gmail.com>
2023-12-05 20:39:23 +0100

    br line in name

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", function () {
     if (window.scrollY > 50) {
       document.getElementById("navbar_top").classList.add("fixed-top");
       // add padding top to show content behind navbar
-      // navbar_height = document.querySelector(".navbar").offsetHeight;
-      // document.body.style.paddingTop = navbar_height + "px";
+      navbar_height = document.querySelector(".navbar").offsetHeight;
+      document.body.style.paddingTop = navbar_height + "px";
     } else {
       document.getElementById("navbar_top").classList.remove("fixed-top");
       // remove padding top from body
-      // document.body.style.paddingTop = "0";
+      document.body.style.paddingTop = "0";
     }
   });
 });
